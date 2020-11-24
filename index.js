ee4d0720716e90b8cc831fb44e7451787f35f27b
ts-miller <tylerstephen11@gmail.com>
2020-11-24 16:01:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+function renderUser(obj) {
+    
+}
+
+
+function submitData(name, email) {
+    let formData = {
+        name,
+        email
+    }
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+    return fetch('http://localhost:3000/users', configObj)
+            .then(function(resp) {
+             return resp.json()
+            })
+            .then(function(obj) {
+                document.body.innerHTML = obj.id
+            })
+            .catch(error => {
+                document.body.innerHTML = error.message
+            })
+}
+
